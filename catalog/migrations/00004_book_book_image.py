2ff58e1ca367593293f4f260964ed4f8251dd424
SDF-Git <tylerfrancis@unomaha.edu>
2026-03-29 23:22:58 -0500

    PythonAnywhere

---
+++ catalog/migrations/00004_book_book_image.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalog', '0003_author_author_image'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='book',
+            name='book_image',
+            field=models.ImageField(blank=True, null=True, upload_to='images/'),
+        ),
+    ]
